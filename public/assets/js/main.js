f68db729cf1a55133c0224b2e921a79ded85a945
nebula <nebula@orbitalsolutions.ca>
2024-07-21 21:35:02 -0300

    Update carousel and scrollable list layout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -64,166 +64,168 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const toolImages = [
     {
-      image: "AIO Tool 2.5.2 Cracked By Nebula",
+      image: "AIO Tool 2.5.2 Cracked By Nebula.png",
       downloadLink: "./tools/AIO Tool v2.5.2 Cracked By Nebula.rar",
     },
     {
-      image: "Alcatraz Cracked By Nebula",
-      downloadLink:
-        "https://cdn.discordapp.com/attachments/975730799112503306/1261754476264558713/Alcatraz_Cracked_By_Nebula.rar?ex=66941bed&is=6692ca6d&hm=bb0ae5ab41b012532163ae7d108c3d598e290b0674c42cfb8fe536df7758b5ca&",
+      image: "Alcatraz Cracked By Nebula.png",
+      downloadLink: "https://files.catbox.moe/mou7yl.rar",
     },
     {
-      image: "Aurora exclusive Cracked By Nebula",
+      image: "Aurora exclusive Cracked By Nebula.png",
       downloadLink: "./tools/Aurora Cracked By Nebula.rar",
     },
     {
-      image: "BatMan Tool Cracked By Nebula",
+      image: "BatMan Tool Cracked By Nebula.png",
       downloadLink: "./tools/BatMan Tool Cracked By Nebula.rar",
     },
     {
-      image: "BO1 XRPC Cracked By Nebula",
+      image: "BO1 XRPC Cracked By Nebula.png",
       downloadLink: "./tools/BO1_XRPC_Cracked_By_Nebula.zip",
     },
     {
-      image: "Casper 4.1 Cracked By Nebula",
+      image: "Casper 4.1 Cracked By Nebula.png",
       downloadLink: "./tools/Casper 4.1 AUTH Cracked By Nebula.rar",
     },
     {
-      image: "CFW Grabber Cracked By Nebula",
+      image: "CFW Grabber Cracked By Nebula.png",
       downloadLink: "./tools/CFW Grabber.rar",
     },
     {
-      image: "Chaotic360 Cracked By Nebula",
-      downloadLink:
-        "https://cdn.discordapp.com/attachments/975730799112503306/1261754478240202752/Chaotic360_Remastered_v2.0.7_Cracked_By_Nebula.rar?ex=66941bed&is=6692ca6d&hm=d8caa1eb568781f654d1698847fad7e84bdddf5e7e2f5ac87def68443e8c7673&",
+      image: "Chaotic360 Cracked By Nebula.png",
+      downloadLink: "https://files.catbox.moe/e4vefh.rar",
     },
     {
-      image: "DankTool Cracked By Nebula",
+      image: "DankTool Cracked By Nebula.png",
       downloadLink: "./tools/DANK TOOL Cracked By Nebula.rar",
     },
     {
-      image: "Dank RGH Grabber Cracked By Nebula",
+      image: "Dank RGH Grabber Cracked By Nebula.png",
       downloadLink: "./tools/Dank RGH Grabber Cracked By Nebula.rar",
     },
     {
-      image: "DeathRGH Tools Studio Cracked By Nebula",
+      image: "DeathRGH Tools Studio Cracked By Nebula.png",
       downloadLink: "./tools/DeathRGH Tool Private Cracked By Nebula.rar",
     },
     {
-      image: "Halo Astro 2 Cracked By Nebula",
+      image: "Halo Astro 2 Cracked By Nebula.png",
       downloadLink: "./tools/Halo Astro 2 Cracked By Nebula.rar",
     },
     {
-      image: "Imperium Cracked By Nebula",
+      image: "Imperium Cracked By Nebula.png",
       downloadLink:
         "./tools/1.27_IMPERIUM_ACCOUNT_EDITOR_0.9 Cracked By Nebula.rar",
     },
     {
-      image: "Injector Cracked By Nebula",
+      image: "Injector Cracked By Nebula.png",
       downloadLink: "./tools/Injector_Cracked_By_Nebula.zip",
     },
     {
-      image: "Nimbus Cracked By Nebula",
+      image: "Nimbus Cracked By Nebula.png",
       downloadLink: "./tools/Nimbus Cracked By Nebula.rar",
     },
     {
-      image: "Paragon Engine Cracked By Nebula",
+      image: "Paragon Engine Cracked By Nebula.png",
       downloadLink: "./tools/Paragon Engine Cracked By Nebula.rar",
     },
     {
-      image: "Pesticide Cracked By Nebula",
+      image: "Pesticide Cracked By Nebula.png",
       downloadLink: "./tools/Pesticide Beta 1.0.0 Cracked By Nebula.rar",
     },
     {
-      image: "Project Nix Cracked By Nebula",
+      image: "Project Nix Cracked By Nebula.png",
       downloadLink: "./tools/NIX Cracked By Nebula.rar",
     },
     {
-      image: "Project Rebirth Cracked By Nebula",
+      image: "Project Rebirth Cracked By Nebula.png",
       downloadLink: "./tools/Project Rebirth Cracked By Nebula.rar",
     },
     {
-      image: "Project Stealth Cracked By Nebula",
+      image: "Project Stealth Cracked By Nebula.png",
       downloadLink: "./tools/Project Stealth Cracked By Nebula.rar",
     },
     {
-      image: "Revox Cracked By Nebula",
+      image: "Revox Cracked By Nebula.png",
       downloadLink: "./tools/Revox v3 Cracked By Nebula.rar",
     },
     {
-      image: "Satan's Modding Team Tool Cracked By Nebula",
-      downloadLink:
-        "https://cdn.discordapp.com/attachments/975730799112503306/1261783082545971312/Satan_Modding_Tool_V6.2.2_Cracked_By_Nebula.rar?ex=66943691&is=6692e511&hm=5f97306a749b04f5f41c0df0601962dc45f3c94a1085bd35155ecad2bffa387b&",
+      image: "Satan's Modding Team Tool Cracked By Nebula.png",
+      downloadLink: "https://files.catbox.moe/r6m9s6.rar",
     },
     {
-      image: "Simplicity Beta [v0.1] Cracked By Nebula",
+      image: "Simplicity Beta [v0.1] Cracked By Nebula.png",
       downloadLink: "./tools/Simplicity Beta [v0.1] Cracked By Nebula.rar",
     },
     {
-      image: "Solution Finale Cracked By Nebula",
+      image: "Solution Finale Cracked By Nebula.png",
       downloadLink: "./tools/Solution Finale 1.2.0.rar",
     },
     {
-      image: "Super Shitty Tool Cracked By Nebula",
+      image: "Super Shitty Tool Cracked By Nebula.png",
       downloadLink: "./tools/Super Shitty Tool Cracked By Nebula.rar",
     },
     {
-      image: "TCM's Multi-COD Tool Cracked By Nebula",
-      downloadLink:
-        "https://cdn.discordapp.com/attachments/975730799112503306/1261783116545134722/TCMs_Multi-COD_Tool_Cracked_By_Nebula.rar?ex=66943699&is=6692e519&hm=fc3c44951ebe5b45b2475ae0061967be73c15ca63c86094a02a0ecea4111f533&",
+      image: "TCM's Multi-COD Tool Cracked By Nebula.png",
+      downloadLink: "https://files.catbox.moe/1w8jkh.rar",
     },
     {
-      image: "The Legion Cracked By Nebula",
+      image: "The Legion Cracked By Nebula.png",
       downloadLink: "./tools/Legion Cracked By Nebula.rar",
     },
     {
-      image: "VENOM Cracked By Nebula",
+      image: "VENOM Cracked By Nebula.png",
       downloadLink: "./tools/VENOM Cracked By Nebula.rar",
     },
     {
-      image: "Versa Shotz Cracked By Nebula",
-      downloadLink:
-        "https://cdn.discordapp.com/attachments/975730799112503306/1261783082974056519/Versa_Shotz_1.3_Cracked_By_Nebula.rar?ex=66943691&is=6692e511&hm=0192d855ad62af2d7a4fa84e177d0eddc1ec9a12d2278bdf3315d962d2c9b220&",
+      image: "Versa Shotz Cracked By Nebula.png",
+      downloadLink: "https://files.catbox.moe/lfoj05.rar",
     },
     {
-      image: "Ziimze Tool Cracked By Nebula",
+      image: "Ziimze Tool Cracked By Nebula.png",
       downloadLink: "./tools/Ziimze Tool Cracked By Nebula.rar",
     },
     {
-      image: "Real Nigga Tool Cracked By Nebula",
+      image: "Real Nigga Tool Cracked By Nebula.png",
       downloadLink: "./tools/My Real Nigga Tool Cracked By Nebula.rar",
     },
     {
-      image: "Project Gucci Cracked By Nebula",
+      image: "Project Gucci Cracked By Nebula.png",
       downloadLink: "./tools/Project Gucci Remastered Cracked By Nebula.rar",
     },
     {
-      image: "BO2 Derank Tool Cracked By Nebula",
+      image: "BO2 Derank Tool Cracked By Nebula.png",
       downloadLink: "./tools/X360DeRank Tool v5.0.6 Cracked By Nebula.rar",
     },
     {
-      image: "TCM BO2 IP Spoofer Cracked By Nebula",
+      image: "TCM BO2 IP Spoofer Cracked By Nebula.png",
       downloadLink: "./tools/TCMipSpoofer+MoreUpdate Cracked By Nebula.rar",
     },
     {
-      image: "Revolution Cracked By Nebula",
+      image: "Revolution Cracked By Nebula.png",
       downloadLink: "./tools/Revolution Tool Cracked By Nebula.rar",
+    },
+    {
+      image: "Notoriety Cracked.png",
+      downloadLink: "https://files.catbox.moe/gnofi9.rar",
+    },
+    {
+      image: "TCM 38.0 Cracked.png",
+      downloadLink: "https://files.catbox.moe/9ffkui.rar",
+    },
+    {
+      image: "GuidoCT V1 Cracked.png",
+      downloadLink: "https://files.catbox.moe/yx4cst.rar",
     },
   ];
 
-  // createCarousel(
-  //   ".carousel",
-  //   "https://nebulamods.ca/assets/img/tools",
-  //   toolImages,
-  //   true
-  // );
+  createCarousel(".carousel", "./assets/img/tools", toolImages, true);
 
   const toolLinksContainer = document.getElementById("tool-links");
   toolImages.forEach((imageData) => {
     const listItem = document.createElement("li");
     const link = document.createElement("a");
     link.href = imageData.downloadLink;
-    link.textContent = imageData.image;
+    link.textContent = imageData.image.replace(".png", "");
     link.classList.add("text-blue-400", "hover:underline");
     link.setAttribute("download", imageData.downloadLink.split("/").pop());
     link.setAttribute("type", (type = "application/x-rar-compressed"));
